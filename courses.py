7013f862db0dd13603f5e7df7e2758fab5dc5a63
Klovborg321 <klovborg123@gmail.com>
2025-06-14 19:19:29 +0200

    Update courses.py

--- courses.py
+++ courses.py
@@ -21,3 +21,139 @@ COURSES = [
     "Holiday Hideaway Hard", "Mount Olympus Easy", "Mount Olympus Hard",
     "Raptor Cliff's Easy", "Raptor Cliff's Hard"
 ]
+COURSE_IMAGES = {
+    "Tourist Trap Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433690077069404/IMG_3888.PNG",
+    "Tourist Trap Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433690077069404/IMG_3888.PNG",
+    "Cherry Blossom Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433690647756811/IMG_3889.PNG",
+    "Cherry Blossom Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433690647756811/IMG_3889.PNG",
+    "Seagull Stacks Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433691251605564/IMG_3890.PNG",
+    "Seagull Stacks Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433691251605564/IMG_3890.PNG",
+    "Arizona Modern Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433691826360360/IMG_3891.PNG",
+    "Arizona Modern Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433691826360360/IMG_3891.PNG",
+    "Original Gothic Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433692392587315/IMG_3892.PNG",
+    "Original Gothic Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433692392587315/IMG_3892.PNG",
+    "Bogey's Bonanza Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433692954361937/IMG_3893.PNG",
+    "Bogey's Bonanza Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433692954361937/IMG_3893.PNG",
+    "Tethys Station Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433693319401573/IMG_3894.PNG",
+    "Tethys Station Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433693319401573/IMG_3894.PNG",
+    "Quixote Valley Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433742514393228/IMG_3895.PNG",
+    "Quixote Valley Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433742514393228/IMG_3895.PNG",
+    "Shangri-La Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433742879162440/IMG_3896.PNG",
+    "Shangri-La Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433742879162440/IMG_3896.PNG",
+    "Sweetopia Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433743319830528/IMG_3897.PNG",
+    "Sweetopia Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433743319830528/IMG_3897.PNG",
+    "20.000 Leagues Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433743554707587/IMG_3898.PNG",
+    "20.000 Leagues Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433743554707587/IMG_3898.PNG",
+    "Upside Town Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433744003235900/IMG_3899.PNG",
+    "Upside Town Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433744003235900/IMG_3899.PNG",
+    "Laser Lair Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433744288579685/IMG_3900.PNG",
+    "Laser Lair Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433744288579685/IMG_3900.PNG",
+    "Alfheim Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433744603025458/IMG_3901.PNG",
+    "Alfheim Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433744603025458/IMG_3901.PNG",
+    "Gardens of Babylon Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433785975640085/IMG_3902.PNG",
+    "Gardens of Babylon Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433785975640085/IMG_3902.PNG",
+    "Labyrinth Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433786244337704/IMG_3903.PNG",
+    "Labyrinth Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433786244337704/IMG_3903.PNG",
+    "El Dorado Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433786504253471/IMG_3904.PNG",
+    "El Dorado Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433786504253471/IMG_3904.PNG",
+    "Myst Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433786743193651/IMG_3905.PNG",
+    "Myst Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433786743193651/IMG_3905.PNG",
+    "Temple of Zerzura Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433787070615644/IMG_3906.PNG",
+    "Temple of Zerzura Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433787070615644/IMG_3906.PNG",
+    "Atlantis Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433787385053194/IMG_3907.PNG",
+    "Atlantis Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433787385053194/IMG_3907.PNG",
+    "Ice Lair Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433787661750303/IMG_3908.PNG",
+    "Ice Lair Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433787661750303/IMG_3908.PNG",
+    "8-Bit Lair Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433830582190181/IMG_3909.PNG",
+    "8-Bit Lair Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433830582190181/IMG_3909.PNG",
+    "Widows's Walkabout Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433830922059846/IMG_3910.PNG",
+    "Widows's Walkabout Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433830922059846/IMG_3910.PNG",
+    "Meow Wolf Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433831299420201/IMG_3911.PNG",
+    "Meow Wolf Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433831299420201/IMG_3911.PNG",
+    "Mars Gardens Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433831685427213/IMG_3912.PNG",
+    "Mars Gardens Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433831685427213/IMG_3912.PNG",
+    "Viva Las Elvis Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433832008384563/IMG_3913.PNG",
+    "Viva Las Elvis Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433832008384563/IMG_3913.PNG",
+    "Around the World Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433832297795664/IMG_3914.PNG",
+    "Around the World Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433832297795664/IMG_3914.PNG",
+    "Journey Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433832578547844/IMG_3915.PNG",
+    "Journey Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433832578547844/IMG_3915.PNG",
+    "Venice Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433878934257775/IMG_3916.PNG",
+    "Venice Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433878934257775/IMG_3916.PNG",
+    "Wallace & Gromit Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433879257223259/IMG_3917.PNG",
+    "Wallace & Gromit Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433879257223259/IMG_3917.PNG",
+    "Holiday Hideaway Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433879617929328/IMG_3918.PNG",
+    "Holiday Hideaway Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433879617929328/IMG_3918.PNG",
+    "Mount Olympus Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433879965929663/IMG_3919.PNG",
+    "Mount Olympus Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433879965929663/IMG_3919.PNG",
+    "Raptor Cliff's Easy":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433880272244846/IMG_3920.PNG",
+    "Raptor Cliff's Hard":
+    "https://cdn.discordapp.com/attachments/1378860910310854666/1379433880272244846/IMG_3920.PNG"
+
+    # Add the rest as needed
+}
